e3c7ae19dbf9c6ed16747b6aa8e0b395d8c6987f
LHYiii <17083702d@gmail.com>
2019-03-25 21:47:16 +0800

    commit from Haoyang

--- Test_For_Project.sql
+++ Test_For_Project.sql
@@ -1,20 +1,31 @@
+delete from Locations;
+delete from Tasks;
+delete from Contracts;
+delete from Customers;
+delete from Specializations;
+delete from PaymentMethods;
+delete from ContactMethods;
+delete from Freelancers;
+delete from Reviews;
+delete from Supervisors;
+
 insert into Locations (lid, lname) values
 (1, 'A'),
 (2, 'B'),
 (3, 'C');
 
 insert into Tasks (tid, description, tdate, lid) values
-(01, 'Hard', 2019-09-01,1),
-(02, 'Easy', 2019-08-02,2),
-(03, 'Intermediate', 2019-11-10,3),
-(04, 'Super Hard', 2019-09-01, 1),
-(05, 'Easy', 2019-09-01, 2),
-(06, 'Intermediate', 2018-12-19, 1);
+(01, 'Hard', '2019-09-01',1),
+(02, 'Easy', '2019-08-02',2),
+(03, 'Intermediate', '2019-11-10',3),
+(04, 'Super Hard', '2019-09-01', 1),
+(05, 'Easy', '2019-09-01', 2),
+(06, 'Intermediate', '2018-12-19', 1);
 
 insert into Contracts (Cid, Date, PayAmount, Tid) values
-(101, 2018-05-27, 500, 01),
-(102, 2018-02-03, 1000, 02),
-(103, 2018-12-19, 200, 03);
+(101, '2018-05-27', 500, 01),
+(102, '2018-02-03', 1000, 02),
+(103, '2018-12-19', 200, 03);
 
 insert into Customers (cname, firstname, lastname, gender) values
 ('Jiang Yichen', 'Yichen', 'Jiang', 'M'),
@@ -26,9 +37,9 @@ insert into Specializations (Specid, DifficultyLevel, Category, Tid) values
 (3, 'B', 'Cooking', 04);
 
 insert into PaymentMethods (cord, cardnumber, expdate, cname) values
-('Credit', '1111', 2020-02-02, 'Jiang Yichen'),
-('Debit', '2222', 2020-03-02, 'Jiang Yichen'),
-('Debit', '3333', 2020-02-02, 'Luna Tao');
+('Credit', '1111', '2020-02-02', 'Jiang Yichen'),
+('Debit', '2222', '2020-03-02', 'Jiang Yichen'),
+('Debit', '3333', '2020-02-02', 'Luna Tao');
 
 insert into ContactMethods (phonenumber, areacode) values
 ('1234567', '11111'),
@@ -39,8 +50,8 @@ insert into Freelancers (fname, Name, Gender, PhonNumber, AreaCode, Cid, Tid, Sp
 ('No.2', 'Haoyang Li', 'M', '7654321', '22222', 103, 03, 2);
 
 insert into Reviews (cname, fname, rating, Date) values
-('Jiang Yichen', 'No.1', 'A', 2019-12-31),
-('Luna Tao', 'No.2', 'B', 2019-12-31);
+('Jiang Yichen', 'No.1', 'A', '2019-12-31'),
+('Luna Tao', 'No.2', 'B', '2019-12-31');
 
 insert into Supervisors (sid, sname, sgender, tid) values
 (1, 'Chan', 'M', 02),
