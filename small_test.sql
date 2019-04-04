d8f2cece07837333a3888875da8f6a7fc76e75c1
yjiang0929 <pauljyc0929@gmail.com>
2019-04-05 00:22:04 +0800

    clickable table and sign contract page

---
+++ small_test.sql
@@ -0,0 +1,11 @@
+insert into customers values ('man','dude','guy','male');
+insert into locations values (1,'singapore');
+insert into specializations values (1, '1','1',1);
+insert into tasks values (1,'this task','2019-04-02',1,1,'man');
+insert into freelancers values ('pikachu','jane','doe','female',null,null,1);
+insert into freelancers values ('pichu','john','doe','male',null,null,1);
+
+insert into bidtasks values ('pikachu',1,1000);
+insert into bidtasks values ('pichu',1,100);
+
+insert into reviews values ('man','pikachu',5,'good','20190404');
