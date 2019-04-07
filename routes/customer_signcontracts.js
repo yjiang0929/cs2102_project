502a0de98b39effcd6b995b5c83bfc2f0029b95f
yjiang0929 <pauljyc0929@gmail.com>
2019-04-07 11:15:42 +0800

    remove redudant files

--- routes/customer_signcontracts.js
+++ routes/customer_signcontracts.js
@@ -15,10 +15,29 @@ const pool = new Pool({
 router.get('/', function(req, res, next) {
   let tid = req.query.tid;
   var sql_query = 'SELECT * FROM bidtasks WHERE tid=' + tid
-  console.log(sql_query);
 
   pool.query(sql_query, (err, data) => {
     res.render('customer_signcontracts', { title: 'CS2102 Project', data:data.rows});
   });
 });
+
+var contract_insert = "INSERT INTO contracts VALUES";
+
+router.post('/',function(req, res, next){
+  var cid = req.body.cid;
+  var fname = req.body.fname;
+  var tid = "2";
+  var date = "'20190201'";
+  var payamount = "100";
+  var cname = "'man'"
+  console.log("tid");
+
+  var contract_insert_query = contract_insert + "(" + cid + "," + date + "," + payamount + "," + tid + ",'" + fname + "'," + cname + ")";
+  console.log(contract_insert_query);
+
+  pool.query(contract_insert_query, (err, data) => {
+    res.redirect('/customer_index')
+  });
+});
+
 module.exports = router;
