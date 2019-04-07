028dd41b0a12696b739cb3f4af6aecfbcff80aab
Luna Tao <lunatao@Lunas-MacBook-Pro.local>
2019-04-07 14:18:31 +0800

    supervisor update

--- routes/supervisor_view_my_contracts.js
+++ routes/supervisor_view_my_contracts.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'cs2102',
+  password: '********',
+  port: 5432,
+})
+
+var sql_query = 'SELECT * FROM Contracts'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('supervisor_view_my_contracts', { title: 'CS2102 Project' });
+	pool.query(sql_query, (err, data) => {
+  		res.render('supervisor_view_my_contracts', { title: 'CS2102 Project', data:data.rows });
+	});
 });
 
 module.exports = router;
