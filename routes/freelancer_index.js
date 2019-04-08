6beb5a8a5adcdea89f8868c5e511efb50745165f
sunlicheng <lsun457@aucklanduni.ac.nz>
2019-04-08 21:32:57 +0800

    freelancer index changes

--- routes/freelancer_index.js
+++ routes/freelancer_index.js
@@ -10,14 +10,26 @@ const pool = new Pool({
   port: 5432,
 })
 
+var contact_query = "SELECT * FROM ContactMethods WHERE name='";
+var contact_insert = 'INSERT INTO ContactMethods VALUES';
 var sql_query = 'SELECT * FROM freelancers'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	  pool.query(sql_query, (err, data) => {
-  res.render('freelancer_index', { title: 'CS2102 Project', data:data.rows});
-	});
-});
+  var name = req.session.user;
+  var new_contact_query = contact_query + name + "'";
 
+	pool.query(sql_query, (err, data) => {
+	pool.query(new_contact_query, (err0, data1) => {   
+        res.render('freelancer_index', { title: 'CS2102 Project', name:name, data:data.rows, cdata:data1.rows});
+    	});  
+	});
+});  
+
+router.post('/',function(req, res, next){
+  var phonenumber = req.body.phonenumber;
+  var areacode = req.body.areacode;
+  var name = req.session.user;
+});
 
 module.exports = router;
